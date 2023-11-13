d41e5c3e72d8d97cc273bc3cd154e4f51b980a9c
Prem Sai <saiprem0585@gmail.com>
2023-11-13 18:37:35 +0530

    Disabled posting attendance on non working days

--- src/components/AttendanceRow.js
+++ src/components/AttendanceRow.js
@@ -4,7 +4,7 @@ export default function AttendanceRow({date, atndObj}) {
         <>
         {atndObj.present && (
             <div className="row atnd-list-row py-3" data-bs-theme="dark">
-                <div className="col-md-4 col-6 text-center">{date}</div>
+                <div className="col-md-3 col-6 text-center">{date}</div>
                 <button className="navbar-toggler d-md-none col-6" type="button" data-bs-toggle="collapse" data-bs-target={`#id-${date}`}
                     aria-controls={`id-${date}`} aria-expanded="true" aria-label="Toggle navigation" >
                     <span className="navbar-toggler-icon"></span>
@@ -12,14 +12,14 @@ export default function AttendanceRow({date, atndObj}) {
                 <div id={`id-${date}`} className="collapse col-md-8 row d-md-none mx-4 text-muted">
                     <div className="col-12"><span className="text-success">In:</span> {moment(atndObj.inTime).format('HH:mm:ss')}</div>
                     <div className="col-12"><span className="text-danger">Out:</span> {moment(atndObj.endTime).format('HH:mm:ss')}</div>
-                    <div className='col-12'><span className="fw-bold">Hours:</span> {((moment(atndObj.endTime).diff(moment(atndObj.inTime), 'minutes'))/60).toFixed(2)}</div>
-                    <div className={`col-12 ${atndObj.late?'text-danger':''}`}>{atndObj.late?'':'Not '}Late</div>
+                    <div className='col-12'><span className="fw-bold">Hours Worked:</span> {((moment(atndObj.endTime).diff(moment(atndObj.inTime), 'minutes'))/60).toFixed(2)}</div>
+                    <div className={`col-12 ${atndObj.late?'text-danger':'text-primary'}`}>{atndObj.late?'':'Not '}Late</div>
                 </div>
-                <div className="d-none d-md-flex col-md-8 row text-center">
+                <div className="d-none d-md-flex col-md-9 row text-center">
                     <div className="col-3"><span className="text-success">In:</span> {moment(atndObj.inTime).format('HH:mm:ss')}</div>
                     <div className="col-3"><span className="text-danger">Out:</span> {moment(atndObj.endTime).format('HH:mm:ss')}</div>
                     <div className='col-3'><span className="fw-bold">Hours Worked:</span> {((moment(atndObj.endTime).diff(moment(atndObj.inTime), 'minutes'))/60).toFixed(2)}</div>
-                    <div className={`col-3 ${atndObj.late?'text-danger':''}`}>{atndObj.late?'':'Not '}Late</div>
+                    <div className={`col-3 ${atndObj.late?'text-danger':'text-primary'}`}>{atndObj.late?'':'Not '}Late</div>
                 </div>
             </div>
         )}
--- src/components/PostAttendance.js
+++ src/components/PostAttendance.js
@@ -7,6 +7,7 @@ import moment from "moment";
 export default function PostAttendance(){
     const [attendaces, setAttendances] = useState({});
     const [reRender, setReRender] = useState(0);
+    const [weekday, setWeekday] = useState(false);
     const postInAttendance = (empID)=>{
         axiosInstance.post('/post/in/atnd', {empID}).then((res)=>{
             if (res.data.success){
@@ -35,31 +36,35 @@ export default function PostAttendance(){
         });
     }
     useEffect(()=>{
-        var todaysAttendance = [];
-        var employees = [];
-        var atndObj = {};
-        axiosInstance.get('/get/todays/atnd').then((res)=>{
-            todaysAttendance = res.data.todaysAttendance;
-            axiosInstance.get('/get/emps').then((res)=>{
-                employees = res.data.employees;
-                employees.forEach((employee) => {
-                    atndObj[employee.empID] = {name: employee.empName,inTime: null, endTime: null};
+        if (parseInt(moment().format('d')) != 0 && parseInt(moment().format('d')) != 6){
+            setWeekday(true);
+            var todaysAttendance = [];
+            var employees = [];
+            var atndObj = {};
+            axiosInstance.get('/get/todays/atnd').then((res)=>{
+                todaysAttendance = res.data.todaysAttendance;
+                axiosInstance.get('/get/emps').then((res)=>{
+                    employees = res.data.employees;
+                    employees.forEach((employee) => {
+                        atndObj[employee.empID] = {name: employee.empName,inTime: null, endTime: null};
+                    })
+                    todaysAttendance.forEach((atnd)=>{
+                        atndObj[atnd.empID].inTime = atnd.inTime;
+                        atndObj[atnd.empID].endTime = atnd.endTime;
+                    })
+                    setAttendances(atndObj);
+                }).catch((error)=>{
+                    alert("Could not fetch employees list. Please try again.")
                 })
-                todaysAttendance.forEach((atnd)=>{
-                    atndObj[atnd.empID].inTime = atnd.inTime;
-                    atndObj[atnd.empID].endTime = atnd.endTime;
-                })
-                setAttendances(atndObj);
             }).catch((error)=>{
-                alert("Could not fetch employees list. Please try again.")
+                alert("Could not fetch today's attendance. Please try again.")
             })
-        }).catch((error)=>{
-            alert("Could not fetch today's attendance. Please try again.")
-        })
+        }
     }, [reRender])
     return (
         <div className="d-flex flex-column min-vh-100">
             <Navbar />
+            { weekday && 
             <div className="container flex-grow-1 d-flex align-items-stretch justify-content-center flex-column my-3">
                 <h3 className="text-center my-3">Post Attendance!</h3>
                 <div className="post-atnd-wrapper px-4 py-2">
@@ -91,6 +96,12 @@ export default function PostAttendance(){
                     })}
                 </div>
             </div>
+             }
+             { !weekday &&  
+                <div className="container flex-grow-1 d-flex align-items-stretch justify-content-center flex-column my-3">
+                <h3 className="text-center my-3">Not a working day!</h3>
+                </div>
+             }
         </div>
     )
 }
